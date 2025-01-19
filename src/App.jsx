d867acafc4021952d5f222d9e04a22afc5a2b713
alfansatoruu <hellb2254@gmail.com>
2025-01-19 21:46:47 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import GaleriUtama from './components/GaleriUtama';
 import Loading from './components/Loading_screen';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,12 +19,11 @@ const App = () => {
       {isLoading ? (
         <Loading />
       ) : (
-        <BrowserRouter basename="/crazy-starbick">
-        <Routes>
-          <Route path="/" element={<GaleriUtama />} />
-        </Routes>
-      </BrowserRouter>
-      
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<GaleriUtama />} />
+          </Routes>
+        </HashRouter>
       )}
     </>
   );
